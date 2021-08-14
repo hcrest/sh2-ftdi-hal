b1d3331dc423a6efc244589477daaa9e1ee21475
David Wheeler <david.wheeler@ceva-dsp.com>
2021-08-14 12:43:04 -0400

    Updated copyright attributions to CEVA, Inc.

--- Rfc1662Framer.cpp
+++ Rfc1662Framer.cpp
@@ -1,11 +1,19 @@
-// =================================================================================================
-//
-// Copyright (c) 2016 Hillcrest Laboratories, Inc.  All rights reserved.
-// HILLCREST PROPRIETARY/CONFIDENTIAL
-//
-// Rfc1662Framer.cpp
-//
-// =================================================================================================
+/*
+ * Copyright 2016-21 CEVA, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License and
+ * any applicable agreements you may have with CEVA, Inc.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 // =================================================================================================
 // INCLUDE FILES
